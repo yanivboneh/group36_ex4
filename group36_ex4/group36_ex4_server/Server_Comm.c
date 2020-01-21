e86bc616cdb0961bf7ed7d63e4335664db65bfdc
Yuval Kuperberg <kupery111@gmail.com>
2020-01-21 13:33:04 +0200

    עוד עבודה על משחק מול השרת

--- group36_ex4/group36_ex4_server/Server_Comm.c
+++ group36_ex4/group36_ex4_server/Server_Comm.c
@@ -9,6 +9,7 @@
 #include "Server_Comm.h"
 #include "Socket_Shared.h"
 #include "Socket_Send_Recv_Tools.h"
+#include "Server_Game.h"
 
 HANDLE ThreadHandles[NUM_OF_WORKER_THREADS];
 SOCKET ThreadInputs[NUM_OF_WORKER_THREADS];
@@ -152,138 +153,7 @@ static void CleanupWorkerThreads()
 		}
 	}
 }
-char* get_server_move_as_string(int server_move) {
-	switch (server_move){
-	case 1:
-		return ROCK;
-	case 2:
-		return PAPER;
-	case 3:
-		return SCISSORS;
-	case 4:
-		return SPOCK;
-	case 5:
-		return LIZARD;
-	default:
-		return "ERROR";
-	}
-}
 
-int who_is_the_winner(char *player1_move, char *player2_move) {
-	int ret_val = -1;
-	if (!strcmp(player1_move, player2_move))
-		ret_val = 0;
-	else if (!strcmp(player1_move, ROCK)){
-		if (!strcmp(player2_move, PAPER) || !strcmp(player2_move, SPOCK))
-			ret_val = 2;
-		else
-			ret_val = 1;
-	}
-	else if (!strcmp(player1_move, PAPER)){
-		if (!strcmp(player2_move, SCISSORS) || !strcmp(player2_move, LIZARD))
-			ret_val = 2;
-		else
-			ret_val = 1;
-	}
-	else if (!strcmp(player1_move, SCISSORS)){
-		if (!strcmp(player2_move, ROCK) || !strcmp(player2_move, SPOCK))
-			ret_val = 2;
-		else
-			ret_val = 1;
-	}
-	else if (!strcmp(player1_move, SPOCK)) {
-		if (!strcmp(player2_move, LIZARD) || !strcmp(player2_move, PAPER))
-			ret_val = 2;
-		else
-			ret_val = 1;
-	}
-	else if (!strcmp(player1_move, LIZARD)){
-		if (!strcmp(player2_move, SCISSORS) || !strcmp(player2_move, ROCK))
-			ret_val = 2;
-		else
-			ret_val = 1;
-	}
-	return ret_val;
-}
-
-
-int play_against_server(SOCKET *server_socket) {
-	time_t t;
-	int random_server_move, error_flag = 0;
-	char *server_move = NULL;
-	TransferResult_t SendRes;
-	TransferResult_t RecvRes;
-	TCHAR *rcv_buffer = NULL;
-	char send_buffer[MAX_MESSAGE_LEN], *AcceptedStr = NULL;;
-	srand((unsigned)time(&t));
-	random_server_move = rand() % 6+1;
-	server_move = get_server_move_as_string(random_server_move);
-	printf("Server: SERVER_PLAYER_MOVE_REQUEST\n");
-	error_flag = send_message_with_length("SERVER_PLAYER_MOVE_REQUEST", NULL, server_socket);
-	if (error_flag == -1) {
-		printf("Service socket error while writing, closing thread.\n");
-		closesocket(*server_socket);
-		return 1;
-	}
-	RecvRes = ReceiveString(&rcv_buffer, *server_socket, "server");
-	if (RecvRes == TRNS_FAILED) {
-		printf("Service socket error occured while reading, closing thread.\n");
-		closesocket(*server_socket);
-		//return 1;
-	}
-	else if (RecvRes == TRNS_DISCONNECTED) {
-		printf("Connection error occured while reading, closing thread.\n");
-		//goto some_error;
-	}
-	printf("Server: rcv_buffer = :%s\n", rcv_buffer);
-	if (who_is_the_winner(get_server_move_as_string(random_server_move), rcv_buffer) == 1) {
-		printf("Server won: %s > %s", get_server_move_as_string(random_server_move), rcv_buffer);
-	}
-	else {
-		printf("Server won: %s > %s", get_server_move_as_string(random_server_move), rcv_buffer);
-	}
-	return error_flag;
-}
-
-int server_game_handler(char *username, SOCKET *server_socket){
-	int error_flag = 0;
-	TransferResult_t SendRes;
-	TransferResult_t RecvRes;
-	TCHAR *rcv_buffer = NULL;
-	char send_message_buffer[MAX_MESSAGE_LEN], send_message_len_buffer[MESSAGE_LEN_AS_INT];
-	char *AcceptedStr = NULL;
-	//printf("Server: SERVER_APPROVED\n");
-	error_flag = send_message_with_length("SERVER_APPROVED", NULL, server_socket);
-	error_flag = send_message_with_length("SERVER_MAIN_MENU", NULL, server_socket);
-	if (error_flag == -1) {
-		printf("Service socket error while writing, closing thread.\n");
-		closesocket(*server_socket);
-		return 1;
-	}
-	while (TRUE) {
-		//*AcceptedStr = NULL;
-		RecvRes = ReceiveString(&rcv_buffer, *server_socket, "server");
-		if (RecvRes == TRNS_FAILED) {
-			printf("Service socket error occured while reading, closing thread.\n");
-			closesocket(*server_socket);
-			//return 1;
-		}
-		else if (RecvRes == TRNS_DISCONNECTED) {
-			printf("Connection error occured while reading, closing thread.\n");
-			//goto some_error;
-		}
-		printf("Server: rcv_buffer = :%s\n", rcv_buffer);
-		printf("Server: Going out of ReceiveString\n");
-		if (STRINGS_ARE_EQUAL(rcv_buffer, "CLIENT_VERSUS")) {
-			//todo
-		}
-		else if (STRINGS_ARE_EQUAL(rcv_buffer, "CLIENT_CPU")) {
-			error_flag = play_against_server(server_socket);
-		}		
-	}
-	free(AcceptedStr);
-	return error_flag;
-}
 
 
 
@@ -296,7 +166,7 @@ static DWORD ServiceThread(SOCKET *t_socket)
 	TCHAR *rcv_buffer = NULL;
 	TransferResult_t RecvRes;
 	RecvRes = ReceiveString(&rcv_buffer, *t_socket, "server");
-	printf("Server: rcv_buffer = :%s\n", rcv_buffer);
+	printf("Server: rcv_buffer = %s\n", rcv_buffer);
 	if (RecvRes == TRNS_FAILED){
 		printf("Service socket error occured while reading, closing thread.\n");
 		closesocket(*t_socket);
@@ -326,11 +196,6 @@ static DWORD ServiceThread(SOCKET *t_socket)
 			error_flag = send_message_with_length("SERVER_APPROVED", NULL, t_socket);
 			error_flag = server_game_handler(username, t_socket);
 		}
-		//if (SendRes == TRNS_FAILED){
-		//	printf("Service socket error while writing, closing thread.\n");
-		//	closesocket(*t_socket);
-		//	return 1;
-		//}	
 	}
 	closesocket(*t_socket);
 	return 0;
